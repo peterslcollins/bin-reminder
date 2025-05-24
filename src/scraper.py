12c03e4b1460f1be3b7f16a3a407ce55a30715b7
petercollins <peterslcollins@gmail.com>
2025-05-24 21:51:25 +1000

    fixed headless browser

--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,6 @@
 # scraper.py
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import ChromiumOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,8 +8,8 @@ import time
 
 
 def scrape_bin_collection(address):
-    options = Options()
-    options.headless = True
+    options = ChromiumOptions()
+    options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
 
     try:
